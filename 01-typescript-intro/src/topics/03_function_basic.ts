a9bebc1cf679d0e78a19d010a18aee5614c037b4
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2023-06-13 15:38:18 -0600

    Funciones con objetos como argumentos

--- 01-typescript-intro/src/topics/03_function_basic.ts
+++ 01-typescript-intro/src/topics/03_function_basic.ts
@@ -26,3 +26,31 @@ function multiply(firstNumber: number, secondNumber?: number, base: number = 2) 
 // const multitplyResult: number = multiply(5);
 // console.log({result, result2, multitplyResult});
 // export {};
+
+// ---------------------------------------------------------------------------
+// FUNCIONES CON OBEJTOS COMO ARGUMENTOS
+
+interface Character {
+    name: string;
+    hp: number;
+    showHp: () => void;
+}
+
+const healCharacter = (character: Character, amount: number) => {
+    character.hp += amount;
+}
+
+const strider: Character = {
+    name: 'Strider',
+    hp: 50,
+    showHp() {
+        console.log(`Puntos de vida ${this.hp}`);
+    },
+}
+
+healCharacter(strider, 10);
+healCharacter(strider, 30);
+
+strider.showHp();
+
+export {};

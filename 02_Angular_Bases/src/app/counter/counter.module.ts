644ca5b86694f85839bb0dd96faa5eec72e653fb
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2023-07-19 14:43:26 -0600

    Módulos en Angular

---
+++ 02_Angular_Bases/src/app/counter/counter.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CounterComponent } from './components/counter/counter.component';
+// Esto es un agrupador de mi module, counter
+
+@NgModule({
+  declarations: [
+    CounterComponent
+  ],
+
+  // Un Modulo solo se puede consumir dentr de su mismo Scope, para que sea visible al mundo exterior, es necesario exportarlo como el siguiente ejemplo.
+  exports: [
+    CounterComponent
+  ]
+})
+export class CounterModule{};

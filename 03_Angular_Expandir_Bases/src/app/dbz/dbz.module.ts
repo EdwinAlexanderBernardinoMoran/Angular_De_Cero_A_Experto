83a49fd33c47cc1fc1e49688a8c452950cd4f8e9
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2023-08-10 14:21:46 -0600

    FormsModule y NgModel: la directiva de Angular, lo que usamos para hacer to-way-data-binding en el form, creamos un metodo en el components de dbz, en add-character

--- 03_Angular_Expandir_Bases/src/app/dbz/dbz.module.ts
+++ 03_Angular_Expandir_Bases/src/app/dbz/dbz.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+// Cuando es Algo propio de Angular siempre se exporta de primero
 import { MainPageComponent } from './pages/main-page.component';
 import { ListComponent } from './components/list/list.component';
 import { AddCharacterComponent } from './components/add-character/add-character.component';
@@ -16,7 +19,9 @@ import { AddCharacterComponent } from './components/add-character/add-character.
     MainPageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    // Importamos el FormsModule ya que lo estamos ocupando aca especificamente
+    FormsModule
   ]
 })
 export class DbzModule { }

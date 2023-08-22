e244b352b0d63a8c8b3ace4d2f6059599979816e
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2023-08-22 17:09:43 -0600

    @ViewChild - Referencia al Html

--- 04-gifs-app/src/app/gifs/components/search-box/search-box.component.ts
+++ 04-gifs-app/src/app/gifs/components/search-box/search-box.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'gifs-search-box',
   template: `
     <h5>Buscar:</h5>
-    <input type="text" class="form-control" placeholder="Buscar gifs...">
+    <!-- <input type="text" class="form-control" placeholder="Buscar gifs..." (keyup.enter)="searchTag(txtTagInput.value)" #txtTagInput> -->
+
+    <input type="text" class="form-control" placeholder="Buscar gifs..." (keyup.enter)="searchTag()" #txtTagInput>
   `
 })
 
 export class SearchBoxComponent{
+
+  @ViewChild('txtTagInput')
+
+  // Non Null Operation
+  // El ! significa que siempre obtendra el valor
+  public tagInput!: ElementRef<HTMLInputElement>;
+  constructor() {}
+
+  // searchTag(newTag: string){
+  //   console.log({newTag});
+  // }
+
+  searchTag(){
+    const newTag = this.tagInput.nativeElement.value
+    console.log({newTag});
+  }
 }

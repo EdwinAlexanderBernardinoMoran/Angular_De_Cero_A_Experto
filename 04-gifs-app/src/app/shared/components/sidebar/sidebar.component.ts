7f344769665a7159c9789feaeedfc5f253d5b61c
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2023-08-24 15:22:23 -0600

    GifsService Implementando el servicio

--- 04-gifs-app/src/app/shared/components/sidebar/sidebar.component.ts
+++ 04-gifs-app/src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GifsService } from 'src/app/gifs/services/gifs.service';
 
 @Component({
   selector: 'shared-sidebar',
@@ -6,4 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
+  constructor(private gifsService:GifsService) {}
+
+  get tags(){
+    // Accediendo a las propiedades del Servicio
+    return this.gifsService.tagsHistory;
+  }
 }

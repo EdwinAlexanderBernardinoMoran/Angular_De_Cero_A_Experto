e81d32ae3b073defacf6a999567776a602116ce0
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2023-09-06 15:19:20 -0600

    Inicializando proyecto de la aplicacion anterior de gifs para la seccion 8 creando un Image loader

---
+++ 05-gifs-app-loader-image/src/app/app.component.spec.ts
@@ -0,0 +1,27 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    declarations: [AppComponent]
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title '05-gifs-app-loader-image'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('05-gifs-app-loader-image');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('05-gifs-app-loader-image app is running!');
+  });
+});
